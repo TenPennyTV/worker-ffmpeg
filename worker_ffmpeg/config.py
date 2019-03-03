9370371e8ee5765d27e086f1eb64803204fdc72e
Jimmy Wahlberg <jimmy@tenpenny.tv>
2019-03-03 11:51:07 +0100

    Switch database

--- worker_ffmpeg/config.py
+++ worker_ffmpeg/config.py
@@ -5,5 +5,6 @@ SPACES_API_SECRET = os.getenv('SPACES_SECRET')
 DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_PW = os.getenv('DB_PASSWORD', 'temppassword')
 DB_USER = os.getenv('DB_USER', 'tempuser')
+DB_PORT = os.getenv('DB_PORT', '5432')
 BUCKET_NAME = 'tenpenny-content'
 ENDPOINT_URL = 'https://ams3.digitaloceanspaces.com'
--- worker_ffmpeg/db.py
+++ worker_ffmpeg/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from . import config
 
-engine = create_engine('postgresql://%s:%s@%s:5432/tenpenny' % (config.DB_USER, config.DB_PW, config.DB_HOST))
+engine = create_engine('postgresql://%s:%s@%s:%s/tenpenny?sslmode=require' % (config.DB_USER, config.DB_PW, config.DB_HOST, config.DB_PORT))
 
 Session = sessionmaker(bind=engine)
 
